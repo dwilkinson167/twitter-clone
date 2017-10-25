8bb1c0a38189eac10901a095f5b1e8b5afdbb9f5
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-10-25 11:19:31 -0700

    more database connecting

---
+++ config/secret.js
@@ -0,0 +1,3 @@
+module.exports = {
+    database: 'mongodb://root:abc123@ds231715.mlab.com:31715/twitterclone'
+}
--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
-
+const User = require('../models/user');
 router.get('/', (req, res, next) => {
     res.render('main/landing');
+});
+
+router.get('/create-new-user', (req, res, next) => {
+    var user = new User();
+    user.email = "test@gmail.com";
+    user.name = "Jack";
+    user.password = "Hello";
+    user.save(function(err) {
+        if (err) return next(err);
+        res.json("Successfully created");
+    });
 });
 
 module.exports = router;
