d355ea0d51f49628d72402332c90bc70668ae084
Dylan Wilkinson <dylanwilkinson@gmail.com>
2017-10-25 09:14:36 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express  = require('express');
+const morgan = require ('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const hbs = require('hbs');
+const expressHbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('.hbs', expressHbs({ defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+
+const mainRoutes = require('./routes/main');
+
+app.use(mainRoutes);
+
+app.listen(3030, (err) => {
+    if (err) console.log(err);
+    console.log(`Running on port ${3030}`)
+});
+
