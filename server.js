ca59e5f4a63672d41058d2efe4a388e276a5c732
Dylan Wilkinson <dwilkinson167@southseattle.edu>
2017-10-26 09:55:09 -0700

    Sessions & Cookies with passport

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const expressHbs = require('express-handlebars');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const flash = require('express-flash');
+const passport = require('passport');
+const cookieParser = require('cookie-parser');
 const config = require('./config/secret');
 
 const app = express();
@@ -31,6 +33,14 @@ app.use(session({
 
 }));
 app.use(flash());
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res, next) {
+    res.locals.user = req.user;
+    next();
+});
+
 
 const mainRoutes = require('./routes/main');
 const userRoutes = require('./routes/user');
