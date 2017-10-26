9f5e55171c8f617fb41d1d3ad41d6a64bf2a4fba
Dylan Wilkinson <dwilkinson167@southseattle.edu>
2017-10-25 17:20:57 -0700

    updated create user route

--- routes/main.js
+++ routes/main.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
+const User = require('../models/user');
 
 router.get('/', (req, res, next) => {
     res.render('main/landing');
+});
+
+router.get('/create-new-user', (req, res, next) => {
+    var user = new User();
+    user.email = "Foobar@gmail.com";
+    user.name = "Jimi";
+    user.password = "Hello";
+    user.save(function(err) {
+        if (err) return next(err);
+        res.json("Successfully created");
+    });
 });
 
 module.exports = router;
