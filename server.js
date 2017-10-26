f0b3b704639a87f0a4d7be0cb0af75bf45e03518
Dylan Wilkinson <dwilkinson167@southseattle.edu>
2017-10-25 18:14:51 -0700

    Added sessions

--- server.js
+++ server.js
@@ -4,8 +4,13 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const hbs = require('hbs');
 const expressHbs = require('express-handlebars');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('express-flash');
 const config = require('./config/secret');
+
 const app = express();
+
 
 mongoose.connect(config.database, function(err) {
    if (err) console.log(err);
@@ -18,6 +23,14 @@ app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(session({
+    resave: true,
+    saveUninitialized: true,
+    secret: config.secret,
+    store: new MongoStore({ url: config.database, autoReconnect: true })
+
+}));
+app.use(flash());
 
 const mainRoutes = require('./routes/main');
 
